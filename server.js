85ac7daebef827aee8d84696c85c3f19fde4f0f6
jcqlng <jcqlngeiger@outlook.com>
2020-07-01 19:07:39 -0400

    Fixed problem with dynamic port assignment for Heroku

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 const routes = require('./app/routes');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static('app/public'));
 
 app.use('/', routes);
 
-app.listen(4000, function(){
-    console.log("Listening on port 4000!")
-});
+app.listen(port, function(){
+    console.log(`Listening on port ${port}!`)
+}
