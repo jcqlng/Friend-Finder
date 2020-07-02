961d33cc237af4f5b33ea5975bcd25d2b8849d7b
jcqlng <jcqlngeiger@outlook.com>
2020-07-01 21:24:05 -0400

    gets list of friends and returns in JSON format

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,9 +1,15 @@
 const routes = require('express').Router();
 const path = require('path');
+const data = require('../data/friends.json');
 
 routes.get('/api/friends', function(req, res){
-    res.status(200).json({message: "all my friends"});
+    const friends = data;
+    res.status(200).json({friends});
 })
+
+routes.post('/api/friends', function(req, res){
+
+});
 
 
 module.exports = routes;
